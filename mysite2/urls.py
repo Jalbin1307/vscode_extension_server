5b86f85d7d0c750206a8ca6d2a4d0124244b3283
Jalbin1307 <az13077@gmail.com>
2021-12-09 12:02:29 +0900

    feat : Add path

--- mysite2/urls.py
+++ mysite2/urls.py
@@ -20,5 +20,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('rest_api_test/',include('rest_api_test.urls')),
-    # path('',views.IndexView.as_view(), name="index"),
+    path('',views.IndexView.as_view(), name="index"),
 ]
